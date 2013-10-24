7d06750ee0dce308cccb3097bdd20571fd5b852f
Max Gfeller <max.gfeller@gmail.com>
2013-10-24 22:47:40 +0200

    Improved algorithm for finding the best music video

--- index.js
+++ index.js
@@ -5,22 +5,49 @@ var youtubeBestVideo = {};
 var _goodDescriptionWords = ['official', 'download', 'itunes', 'pre-order', 'performing'];
 var _badDescriptionWords = ['cover', 'live'];
 
-var _getRating = function(video) {
+var _getRating = function(video, title) {
   var rating = 0;
   
   if(!video.description) return rating;
 
-  for(var i = 0; i < _goodDescriptionWords.length; i++) {
-    if(video.description.toLowerCase().indexOf(_goodDescriptionWords[i]) > -1) {
-      rating++;
+  console.log(video);
+  console.log(title);
+
+  // check if the title matches
+  var parts = title.split(' ');
+  for(var i = 0; i < parts.length; i++) {
+    console.log('part is ' + parts[i]);
+    console.log('index is ' + video.title.indexOf(parts[i]));
+    if(video.title.indexOf(parts[i]) < 0 && parts[i].length > 1) {
+      rating += 3;
+      console.log('+3');
     }
   }
 
-  for(var i = 0; i < _badDescriptionWords.length; i++) {
-    if(video.description.toLowerCase().indexOf(_badDescriptionWords[i]) > -1) {
-      rating--;
-    }
+  // check the length of the title
+  var diff = video.title.length - title.length;
+  if(diff < 0) diff *= -1;
+
+  console.log('diff is ' + diff);
+  if(diff > 4) {
+    rating -= 4;
+  } else {
+    rating += 4;
   }
+
+  console.log('rating is ' + rating);
+
+//  for(var i = 0; i < _goodDescriptionWords.length; i++) {
+//    if(video.description.toLowerCase().indexOf(_goodDescriptionWords[i]) > -1) {
+//      rating++;
+//    }
+//  }
+//
+//  for(var i = 0; i < _badDescriptionWords.length; i++) {
+//    if(video.description.toLowerCase().indexOf(_badDescriptionWords[i]) > -1) {
+//      rating--;
+//    }
+//  }
 
   return rating;
 }
@@ -30,11 +57,12 @@ youtubeBestVideo.findBestVideo = function(title, cb) {
     if(err) cb(err);
 
     var bestOne = null;
-    var bestRating = 1;
+    var bestRating = -10;
 
     results.forEach(function(result, index) {
-      if(_getRating(result) > bestRating) {
+      if(_getRating(result, title) > bestRating) {
         bestOne = result;
+        bestRating = _getRating(result, title);
       }
 
       if(index === results.length - 1) {
