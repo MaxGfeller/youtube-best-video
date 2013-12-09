f6d37f7acc1ae4d7bf45caefd9887dfd1c3ead0a
Max Gfeller <max.gfeller@gmail.com>
2013-12-09 21:01:27 +0100

    Implemented the videoTitleFilter

--- index.js
+++ index.js
@@ -5,9 +5,67 @@ var youtubeBestVideo = {};
 var _goodDescriptionWords = ['official', 'download', 'itunes', 'pre-order', 'performing'];
 var _badDescriptionWords = ['cover', 'live'];
 
+/**
+ * Filter a video based on the video title
+ * @param  object video The video object as it is being return by youtube-search
+ * @param  string title The actual title after which has been searched
+ * @return float        The score between 0 and 1
+ */
+var _videoTitleFilter = function(video, title) {
+  var rating = 0;
+  var titleParts = title.split(' ');
+  var disassembledTitle = title.toLowerCase();
+
+  // Check if every word in the title exists in the video title
+  for(var i = 0; i < titleParts.length; i++) {
+    var titlePart = titleParts[i];
+    if(titlePart.length < 2) continue;
+
+    if(video.title.toLowerCase().indexOf(titlePart.toLowerCase()) > -1) {
+      rating += 1 / titleParts.length;
+      disassembledTitle.replace(titlePart.toLowerCase(), '');
+    }
+  }
+
+  // Check if the video title contains any more words
+  var disassembledParts = disassembledTitle.trim().split(' ');
+  for(var i = 0; i < disassembledParts.length; i++) {
+    var disassembledPart = disassembledParts[i];
+    if(disassembledPart.length > 2) {
+      rating -= 2 * (1 / titleParts.length);
+    }
+  }
+
+  return rating;
+}
+
+var _viewCountFilter = function(video, title) {
+
+}
+
+var _favoritesCountFilter = function(video, title) {
+
+}
+
+var _blacklistWordsFilter = function(video, title) {
+
+}
+
+var _whitelistWordsFilter = function(video, title) {
+
+}
+
+var _authorWhitelistWordsFilter = function(video, title) {
+
+}
+
 var _getRating = function(video, title) {
   var rating = 0;
   
+  console.log(video);
+  process.exit(0);
+
+
   if(!video.description) return rating;
 
   // check if the title matches
